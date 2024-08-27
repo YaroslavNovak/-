4696a4bd6c15f824d6f0a050509967631a6797c8
YaroslavNovak <novachok28@gmail.com>
2024-08-28 00:13:34 +0300

    Add files via upload

--- HomeWork/Lesson_2/js/script.js
+++ HomeWork/Lesson_2/js/script.js
@@ -1,15 +1,75 @@
 // Якщо змінна більше нуля - виведіть true, менше - false
 //Перевірте це на варіантах 1, 0, -3.
 
+/*let x = prompt("Введіть будь-яке число");
+if (x > 0) {
+  alert("true");
+} else {
+  alert("false");
+}*/
+
 // Якщо змінна ="test" - виведіть true,
 //Перевірте це на варіантах 'test', "qwerty", true
+
+/*let y = prompt("Введіть слово test або будь-яке інше слово");
+if (y === "test") {
+  alert("true");
+} else {
+  alert("false");
+}*/
 
 // Якщо змінна більше 10 - відніміть 5,
 //менше - додайте 5, результат виведіть в консоль
 //Перевірте це на варіантах 1, 10, 13.
 
+/*const a = Number("10");
+number = prompt("Введіть будь-яке число");
+const x = Number(number);
+if (x > a) {
+  console.log(x - 5);
+} else {
+  console.log(x + 5);
+}/*
+
 //Зробіть сервіс який отримує число від 1 до 12
 // виведіть місяць який дорівнює числу
 
+/*data = prompt("Введіть будь-яке число від 1 до 12");
+if (data === "1") {
+  console.log("Січень");
+} else if (data === "2") {
+  console.log("Лютий");
+} else if (data === "3") {
+  console.log("Березень");
+} else if (data === "4") {
+  console.log("Квітень");
+} else if (data === "5") {
+  console.log("Травень");
+} else if (data === "6") {
+  console.log("Червень");
+} else if (data === "7") {
+  console.log("Липень");
+} else if (data === "8") {
+  console.log("Серпень");
+} else if (data === "9") {
+  console.log("Вересень");
+} else if (data === "10") {
+  console.log("Жовтень");
+} else if (data === "11") {
+  console.log("Листопад");
+} else if (data === "12") {
+  console.log("Грудеь");
+} else {
+  console.log("Такого місяця не існує!");
+}*/
+
 //Зробіть сервіс який отримує тризначне число
 //Поверніть користувачу сумму цих чисел
+
+/*let number1 = prompt("Введіть будь-яке 1-ше число");
+const a = Number(number1);
+let number2 = prompt("Введіть будь-яке 2-ге число");
+const b = Number(number2);
+let number3 = prompt("Введіть будь-яке 3-тє число");
+const c = Number(number3);
+console.log(Number(a + b + c));*/
